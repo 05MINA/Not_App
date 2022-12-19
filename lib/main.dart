9817070539746db32ba982c68ae68f2ac3a8a15f
Mina Heshmat <Mina.Heshmat@Elamir.com>
2022-12-19 16:45:52 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notapp/simple_bloc_observer.dart';
+import 'package:notapp/views/notes_view.dart';
+import 'constants.dart';
+import 'cubits/notes_cubit/notes_cubit.dart';
+import 'models/note_model.dart';
+
+void main() async {
+  await Hive.initFlutter();
+
+  Bloc.observer = SimpleBlocObserver();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
+
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: const NotesView(),
+      ),
+    );
+  }
+}
+
+
+// void main(){
+//   runApp(const MyApp());
+//theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+// }
